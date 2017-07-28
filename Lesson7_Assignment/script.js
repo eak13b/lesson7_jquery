a6ac13b1dcb8f0c7a0b4469498046265dbb8237c
Emily <eak13b@my.fsu.edu>
2017-07-28 21:33:58 +0000

    lesson 7 second final commit

--- Lesson7_Assignment/script.js
+++ Lesson7_Assignment/script.js
@@ -7,13 +7,13 @@ Filename: script.js
 
 /*global $*/
 
-<script>
+
 
     //displays the next element after the current target
     function display(event) {
-        $(event.currentTarget).next().fadeIn("slow")
+        $(event.currentTarget).next().fadeIn("slow");
     } //end of display
 
     //attach event listener to h3 elements to invoke display function when clicked
     $("h3").click(display);
-</script>
+
